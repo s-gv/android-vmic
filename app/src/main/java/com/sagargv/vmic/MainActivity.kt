0fa86197f0c5d70ed06bd0575d851f6639050585
Sagar Gubbi <sagar.writeme@gmail.com>
2021-01-04 20:07:38 +0530

    Option to end IP/port

--- app/src/main/java/com/sagargv/vmic/MainActivity.kt
+++ app/src/main/java/com/sagargv/vmic/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.sagargv.vmic
 
 import android.Manifest
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.view.*
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.JobIntentService
+
 
 private const val LOG_TAG = "vMic"
 private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
 
 class MainActivity : AppCompatActivity() {
+    var ip = "192.168.29.236"
+    var port = 9009
+
     private var permissionToRecordAccepted = false
     private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
 
@@ -21,6 +29,46 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.new_game -> {
+                showIPDialog()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun showIPDialog() {
+        val layout = LayoutInflater.from(this).inflate(R.layout.dialog_addr, null, false))
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("MSG")
+        builder.setTitle("Title")
+        builder.setView(layout)
+        builder.apply {
+            setPositiveButton(android.R.string.ok,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        ip = layout.findViewById<EditText>(R.id.ip).toString()
+                        port = layout.findViewById<EditText>(R.id.port).toString().toInt()
+
+                        Log.e("TAG", ip + ":" + port)
+                    })
+            setNegativeButton(android.R.string.cancel,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User cancelled the dialog
+                    })
+        }
+
+        builder.show()
     }
 
     fun onMicButtonClick(v: View) {
